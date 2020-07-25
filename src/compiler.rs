e2da393f03e2c4420f58802695252c00f16934f3
Ben Konz <benkonz@protonmail.com>
2020-07-25 13:27:01 -0400

    refactoring

--- src/compiler.rs
+++ src/compiler.rs
@@ -1,27 +1,144 @@
-pub fn compile() {
+use inkwell::builder::Builder;
+use inkwell::context::Context;
+use inkwell::module::Linkage;
+use inkwell::module::Module;
+use inkwell::targets::{
+    CodeModel, FileType, InitializationConfig, RelocMode, Target, TargetMachine,
+};
+use inkwell::values::PointerValue;
+use inkwell::{AddressSpace, OptimizationLevel};
 
+pub struct Compiler<'ctx> {
+    pub context: &'ctx Context,
+    pub module: Module<'ctx>,
+    pub builder: Builder<'ctx>,
 }
 
-fn compile_add_ptr() {
+impl<'ctx> Compiler<'ctx> {
+    pub fn init_targets() {
+        Target::initialize_all(&InitializationConfig::default());
+    }
 
-}
+    pub fn compile(&self, program: &[u8]) -> Result<(), String> {
+        let (data, ptr) = self.build_main();
+        self.build_calloc(&data, &ptr)?;
 
-fn compile_add() {
+        let mut pc = 0;
+        while pc < program.len() {
+            match program[pc] as char {
+                '>' => self.build_add_ptr(1, &ptr),
+                '<' => self.build_add_ptr(1, &ptr),
+                '+' => self.build_add(1),
+                '-' => self.build_add(-1),
+                '.' => self.build_put(),
+                ',' => self.build_get(),
+                '[' => self.build_while_start(),
+                ']' => self.build_while_end(),
+                _ => (),
+            }
+            pc += 1;
+        }
+        self.build_free(&data);
+        self.return_zero();
 
-}
+        Ok(())
+    }
 
-fn compile_get() {
+    fn build_main(&self) -> (PointerValue, PointerValue) {
+        let i32_type = self.context.i32_type();
+        let main_fn_type = i32_type.fn_type(&[], false);
+        let main_fn_val = self
+            .module
+            .add_function("main", main_fn_type, Some(Linkage::External));
+        let basic_block = self.context.append_basic_block(main_fn_val, "entry");
+        self.builder.position_at_end(basic_block);
 
-}
+        let i8_type = self.context.i8_type();
+        let i8_ptr_type = i8_type.ptr_type(AddressSpace::Generic);
 
-fn compile_put() {
+        let data = self.builder.build_alloca(i8_ptr_type, "data");
+        let ptr = self.builder.build_alloca(i8_ptr_type, "ptr");
 
-}
+        (data, ptr)
+    }
 
-fn compile_while_start() {
+    fn build_calloc(&self, data: &PointerValue, ptr: &PointerValue) -> Result<(), String> {
+        let i64_type = self.context.i64_type();
+        let i8_type = self.context.i8_type();
+        let i8_ptr_type = i8_type.ptr_type(AddressSpace::Generic);
+        let i64_memory_size = i64_type.const_int(30_000, false);
+        let i64_element_size = i64_type.const_int(1, false);
 
-}
+        let calloc_fn_type = i8_ptr_type.fn_type(&[i64_type.into(), i64_type.into()], false);
+        let calloc_fn_val =
+            self.module
+                .add_function("calloc", calloc_fn_type, Some(Linkage::External));
 
-fn compile_while_end() {
+        let data_ptr = self.builder.build_call(
+            calloc_fn_val,
+            &[i64_memory_size.into(), i64_element_size.into()],
+            "calloc_call",
+        );
+        let data_ptr_result: Result<_, _> = data_ptr.try_as_basic_value().flip().into();
+        let data_ptr_basic_val =
+            data_ptr_result.map_err(|_| "calloc returned void for some reason!")?;
 
+        self.builder.build_store(*data, data_ptr_basic_val);
+        self.builder.build_store(*ptr, data_ptr_basic_val);
+
+        Ok(())
+    }
+
+    // TODO: figure out how to have negative numbers
+    fn build_add_ptr(&self, amount: u64, ptr: &PointerValue) {
+        let i32_type = self.context.i32_type();
+        let i32_one = i32_type.const_int(amount, false);
+        let incremented =
+            self.builder
+                .build_int_add(ptr.const_to_int(i32_type), i32_one, "add to data ptr");
+        self.builder.build_store(*ptr, incremented);
+    }
+
+    fn build_add(&self, amount: i32) {}
+
+    fn build_get(&self) {}
+
+    fn build_put(&self) {}
+
+    fn build_while_start(&self) {}
+
+    fn build_while_end(&self) {}
+
+    fn build_free(&self, data: &PointerValue) {
+        self.builder
+            .build_free(self.builder.build_load(*data, "load").into_pointer_value());
+    }
+
+    fn return_zero(&self) {
+        let i32_type = self.context.i32_type();
+        let i32_zero = i32_type.const_int(0, false);
+        self.builder.build_return(Some(&i32_zero));
+    }
+
+    pub fn write_to_file(&self, output_filename: &str) -> Result<(), String> {
+        let target_triple = TargetMachine::get_default_triple();
+        let cpu = TargetMachine::get_host_cpu_name().to_string();
+        let features = TargetMachine::get_host_cpu_features().to_string();
+
+        let target = Target::from_triple(&target_triple).map_err(|e| format!("{:?}", e))?;
+        let target_machine = target
+            .create_target_machine(
+                &target_triple,
+                &cpu,
+                &features,
+                OptimizationLevel::Default,
+                RelocMode::Default,
+                CodeModel::Default,
+            )
+            .ok_or_else(|| "Unable to create target machine!".to_string())?;
+
+        target_machine
+            .write_to_file(&self.module, FileType::Object, output_filename.as_ref())
+            .map_err(|e| format!("{:?}", e))
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,13 @@
 #[macro_use]
 extern crate clap;
 
-mod runtime;
+mod compiler;
 
+use crate::compiler::Compiler;
 use clap::{App, Arg};
 use inkwell::context::Context;
-use inkwell::module::Linkage;
-use inkwell::targets::{
-    CodeModel, FileType, InitializationConfig, RelocMode, Target, TargetMachine
-};
-use inkwell::{OptimizationLevel, AddressSpace};
-use std::env;
-// use std::fs::File;
-// use std::io::prelude::*;
+use std::fs::File;
+use std::io::prelude::*;
 
 fn main() -> Result<(), String> {
     let matches = App::new(crate_name!())
@@ -34,68 +29,22 @@ fn main() -> Result<(), String> {
         )
         .get_matches();
 
+    Compiler::init_targets();
+
     let context = Context::create();
-    let module = context.create_module("brainfrick-rust");
-    let builder = context.create_builder();
+    let compiler = Compiler {
+        context: &context,
+        module: context.create_module("brainfrick-rust"),
+        builder: context.create_builder(),
+    };
 
-    let i32_type = context.i32_type();
-    let main_fn_type = i32_type.fn_type(&[], false);
-    let main_fn_val = module.add_function("main", main_fn_type, Some(Linkage::External));
-    
-    let basic_block = context.append_basic_block(main_fn_val, "entry");
-    builder.position_at_end(basic_block);
+    let source_filename = matches.value_of("INPUT").unwrap();
+    let mut f = File::open(source_filename).map_err(|e| format!("{:?}", e))?;
+    let mut program = Vec::new();
+    f.read_to_end(&mut program)
+        .map_err(|e| format!("{:?}", e))?;
 
-    let i8_type = context.i8_type();
-    let i8_ptr_type = i8_type.ptr_type(AddressSpace::Generic);
-
-    let data = builder.build_alloca(i8_ptr_type, "data");
-    let ptr = builder.build_alloca(i8_ptr_type, "ptr");
-
-    let i64_type = context.i64_type();
-    let i64_memory_size = i64_type.const_int(30_000, false);
-    let i64_element_size = i64_type.const_int(1, false);
-
-    let calloc_fn_type = i8_ptr_type.fn_type(&[i64_type.into(), i64_type.into()], false);
-    let calloc_fn_val = module.add_function("calloc", calloc_fn_type, Some(Linkage::External));
-
-    let data_ptr = builder.build_call(calloc_fn_val, &[i64_memory_size.into(), i64_element_size.into()], "calloc_call");
-    let data_ptr_result: Result<_, _> = data_ptr.try_as_basic_value().flip().into();
-    let data_ptr_basic_val = data_ptr_result.map_err(|_| "calloc returned void for some reason!")?;
-
-    builder.build_store(data, data_ptr_basic_val);
-    builder.build_store(ptr, data_ptr_basic_val);
-
-    // let source_filename = matches.value_of("INPUT").unwrap();
-    // let mut f = File::open(source_filename).map_err(|e| format!("{:?}", e))?;
-    // let mut program = Vec::new();
-    // f.read_to_end(&mut program)
-    //     .map_err(|e| format!("{:?}", e))?;
-
-    builder.build_free(builder.build_load(data, "load").into_pointer_value());
-
-    let i32_zero = i32_type.const_int(0, false);
-    builder.build_return(Some(&i32_zero));
-
-    Target::initialize_all(&InitializationConfig::default());
-
-    let target_triple = TargetMachine::get_default_triple();
-    let cpu = TargetMachine::get_host_cpu_name().to_string();
-    let features = TargetMachine::get_host_cpu_features().to_string();
-
-    let target = Target::from_triple(&target_triple).map_err(|e| format!("{:?}", e))?;
-    let target_machine = target
-        .create_target_machine(
-            &target_triple,
-            &cpu,
-            &features,
-            OptimizationLevel::Default,
-            RelocMode::Default,
-            CodeModel::Default,
-        )
-        .ok_or_else(|| "Unable to create target machine!".to_string())?;
-
+    compiler.compile(&program)?;
     let output_filename = matches.value_of("output").unwrap();
-    target_machine
-        .write_to_file(&module, FileType::Object, output_filename.as_ref())
-        .map_err(|e| format!("{:?}", e))
+    compiler.write_to_file(output_filename)
 }
